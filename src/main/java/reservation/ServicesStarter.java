4309c575e7f1a7e199760761b9725ce058dbbd22
prabum01 <75682228+prabum01@users.noreply.github.com>
2021-07-21 19:55:46 +0800

    Added maven dependencies and spring boot starter

---
+++ src/main/java/reservation/ServicesStarter.java
@@ -0,0 +1,11 @@
+package reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesStarter.class, args);
+    }
+}
